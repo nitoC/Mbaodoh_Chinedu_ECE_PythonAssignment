b38bbf1075b89634e5d3e3259e45bf445e3bb365
nitoc <daren2track@gmail.con>
2022-11-23 18:56:51 +0100

    first commit

---
+++ areaofacircle.py
@@ -0,0 +1,7 @@
+
+pi = 22/7
+print('add radius of a circle')
+inputValue = input()
+radius = float(inputValue)
+areaOFCircle = pi*(radius**2)
+print(areaOFCircle)
---
+++ introduction.py
@@ -0,0 +1,6 @@
+#3
+NameOfP = input("what's your name?:")
+AgeOfP = input("input your age:")
+DeptOfP = input(" what's the name of your department?:")
+
+print("name ="+ NameOfP + "  age = " + AgeOfP + "  department =" +DeptOfP)
